9925eed56dc72b912618647fbc4bfc37b15fb999
寒汀 <zhangxj@tuya.com>
2021-05-28 10:06:19 +0800

    feat: webpack transform

--- app.js
+++ app.js
@@ -1,7 +1,25 @@
+import express from "express";
 import React from "react";
+import { renderToString } from "react-dom/server";
 
 const Page = () => {
   return <h1>This is server page</h1>;
 };
 
-export default Page;
+const app = express();
+const content = renderToString(<Page />);
+
+app.get("/", (req, res) =>
+  res.send(`
+<html>
+   <head>
+       <title>ssr demo</title>
+   </head>
+   <body>
+        ${content}
+   </body>
+</html>
+`)
+);
+
+app.listen(3000, () => console.log("Exampleapp listening on port 3000!"));
